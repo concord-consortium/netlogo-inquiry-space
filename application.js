21a393c6a6223670f84b714c856603533d0205b4
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-05-16 19:52:12 -0400

    update dataGamesProxyPrefix
    http://is-test.kcptech.com/DataGames/Games/concord-github-io
    
    proxies to:
    
    http://concord-consortium.github.io

--- application.js
+++ application.js
@@ -6,7 +6,7 @@ DEVELOPMENT = true;
 var ROOT = "",
     ROOT_REGEX = new RegExp(ROOT + "/.*$"),
     ACTUAL_ROOT = document.location.pathname.replace(ROOT_REGEX, ''),
-    dataGamesProxyPrefix = 'concord-consortium.github.io.netlogo-inquiry-space/';
+    dataGamesProxyPrefix = 'concord-github-io/';
 
 (function() {
 
